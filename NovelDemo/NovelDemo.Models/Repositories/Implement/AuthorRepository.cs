6dcbd3e4fd8368491ccb937c76d1ecce8afb6c27
Kidd Wang <kiddkid99@gmail.com>
2017-09-12 00:52:44 +0800

    Implment AuthorRepository

---
+++ NovelDemo/NovelDemo.Models/Repositories/Implement/AuthorRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NovelDemo.Models.Repositories.Interface;
+using NovelDemo.Models.Repositories.Implement;
+using NovelDemo.Models.Entity;
+using DapperExtensions;
+
+
+namespace NovelDemo.Models.Repositories.Implement
+{
+    public class AuthorRepository : DapperGenericRepository<Author>, IAuthorRepository
+    {
+        public AuthorRepository(IDbConnectionFactory dbConnectionFactory) : base(dbConnectionFactory)
+        {
+            
+        }
+
+        public Author Get(int id)
+        {
+            using (var conn = DbConnectionFactory.Create())
+            {
+                return conn.Get<Author>(id);
+            }
+        }
+    }
+}
--- NovelDemo/NovelDemo.Models/Repositories/Interface/IAuthorRepository.cs
+++ NovelDemo/NovelDemo.Models/Repositories/Interface/IAuthorRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NovelDemo.Models.Entity;
 
 namespace NovelDemo.Models.Repositories.Interface
 {
-    public interface IAuthorRepository
+    public interface IAuthorRepository : IGenericRepository<Author>
     {
+        Author Get(int id);
     }
 }
