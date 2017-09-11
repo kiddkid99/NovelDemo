59407a94059981bf0287b3e840c78c9918b8b6d6
Kidd Wang <kiddkid99@gmail.com>
2017-09-12 00:14:32 +0800

    Add Dapper Extension Add DapperGenericRepository.cs Add Interface IDbConnectionFactory

---
+++ NovelDemo/NovelDemo.Models/Repositories/Implement/DapperGenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using NovelDemo.Models.Repositories.Interface;
+using System.Data.Common;
+
+namespace NovelDemo.Models.Repositories.Implement
+{
+    public class DapperGenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private IDbConnectionFactory DbConnectionFactory { get; }
+
+        public DapperGenericRepository(IDbConnectionFactory dbConnectionFactory)
+        {
+            DbConnectionFactory = dbConnectionFactory;
+        }
+
+        public int Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NovelDemo/NovelDemo.Models/Repositories/Interface/IDbConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace NovelDemo.Models.Repositories.Interface
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection Create();
+    }
+}
