081b7989c978723846a7215d529c4d2fd036cc47
Kidd Wang <kiddkid99@gmail.com>
2017-09-11 23:59:09 +0800

    Add Dapper Add Repository Interface

---
+++ NovelDemo/NovelDemo.Models/Repositories/Interface/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovelDemo.Models.Repositories.Interface
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+
+        int Insert(TEntity entity);
+
+        int Update(TEntity entity);
+
+        int Delete(TEntity entity);
+    }
+}
