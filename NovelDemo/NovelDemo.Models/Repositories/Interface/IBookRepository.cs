668fcdcd5780a7548cd3689b202a1ea440ab59e2
Kidd Wang <kiddkid99@gmail.com>
2017-09-12 01:15:38 +0800

    Update Interface

--- NovelDemo/NovelDemo.Models/Repositories/Interface/IBookRepository.cs
+++ NovelDemo/NovelDemo.Models/Repositories/Interface/IBookRepository.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NovelDemo.Models.Entity;
 
 namespace NovelDemo.Models.Repositories.Interface
 {
-    public interface IBookRepository
+    public interface IBookRepository: IGenericRepository<Book>
     {
+        Book Get(int id);
+
+        IEnumerable<Book> GetAllDetail();
     }
 }
--- NovelDemo/NovelDemo.Models/Repositories/Interface/ICategoryRepository.cs
+++ NovelDemo/NovelDemo.Models/Repositories/Interface/ICategoryRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NovelDemo.Models.Entity;
 
 namespace NovelDemo.Models.Repositories.Interface
 {
-    public interface ICategoryRepository
+    public interface ICategoryRepository : IGenericRepository<Category>
     {
+        Category Get(int id);
     }
 }
