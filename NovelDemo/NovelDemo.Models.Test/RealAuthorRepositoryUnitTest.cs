7c8d0c82222d632d254136def0ca09ce383045c6
Kidd Wang <kiddkid99@gmail.com>
2017-09-12 21:54:32 +0800

    Add RealAuthorRepositoryUnitTest, App.config

---
+++ NovelDemo/NovelDemo.Models.Test/RealAuthorRepositoryUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using NovelDemo.Models.Repositories.Implement;
+using NovelDemo.Models.Repositories.Interface;
+using NovelDemo.Models.Entity;
+
+namespace NovelDemo.Models.Test
+{
+    [TestClass]
+    public class RealAuthorRepositoryUnitTest
+    {
+        private IAuthorRepository repository;
+
+        [TestMethod]
+        public void Test_Inser_Real_Data()
+        {
+            //Arrange
+            string connectionString = ConfigurationManager.ConnectionStrings["NovelDemoDb"].ToString();
+            repository = new AuthorRepository(new DatabaseConnectionFactory(connectionString));
+
+            Author data = new Author() { FirstName = "Wang", LastName = "Kidd", Status = 1, CreateTime = DateTime.Now };
+
+
+            //Action
+            repository.Insert(data);
+
+            //Asset
+            
+        }
+
+
+    }
+}
