4db8c2725db68ed2ac26d6fbc440991cca119884
Kidd Wang <kiddkid99@gmail.com>
2017-09-12 23:29:29 +0800

    Add LayoutController

---
+++ NovelDemo/NovelDemo.Web/Infrastructure/LayoutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NovelDemo.Service.Interface;
+
+namespace NovelDemo.Web.Infrastructure
+{
+    public class LayoutController : Controller
+    {
+        protected readonly IServices Services;
+
+        public LayoutController(IServices services)
+        {
+            Services = services;
+        }
+    }
+}
