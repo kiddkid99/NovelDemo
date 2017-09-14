6761905197d19dff46d5cb26561b598da94832a5
Kidd Wang <kiddkid99@gmail.com>
2017-09-15 00:30:40 +0800

    Add CategoryController

---
+++ NovelDemo/NovelDemo.Web/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NovelDemo.Service.Interface;
+using NovelDemo.Web.Application.Infrastructure;
+using NovelDemo.Web.Application.ViewModel;
+using NovelDemo.Models.Entity;
+
+namespace NovelDemo.Web.Controllers
+{
+    public class CategoryController : LayoutController
+    {
+        public CategoryController(IServices services) : base(services)
+        {
+        }
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
